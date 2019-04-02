3557732a386afbded02df8895a15e6c670e48d17
Mathias Cloet <mathias.cloet.w0624@student.hogent.be>
2019-04-02 21:24:06 +0200

    Line endings

--- AsyncClientServer.Example.Client/MainWindow.xaml.cs
+++ AsyncClientServer.Example.Client/MainWindow.xaml.cs
@@ -44,8 +44,8 @@ namespace AsyncClientServer.Example.Client
 
 		private void StartClient()
 		{
-			//_client = new AsyncClient();
-			_client = new AsyncSslClient("","", TlsProtocol.Tls11);
+			_client = new AsyncClient();
+			//_client = new AsyncSslClient("","", TlsProtocol.Tls11);
 
 			//Bind events
 			_client.ProgressFileReceived += new ProgressFileTransferHandler(Progress);
