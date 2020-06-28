1990306bb66e464c528cfad4d3fd0ac667588ada
cloet <mathias.cloet@hotmail.com>
2020-06-28 14:32:46 +0200

    added message metadata

---
+++ SimpleSockets/Messaging/Metadata/ISessionMetadata.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace SimpleSockets.Messaging.Metadata {
+
+    public interface ISessionMetadata: ISessionInfo, IDisposable {
+
+		new string ClientName { get; set; }
+
+		new Guid Guid { get; set;  }
+
+		new string OsVersion { get; set;  }
+
+		new string UserDomainName { get; set; }
+
+		PacketReceiver DataReceiver { get; }
+
+        SslStream SslStream { get; set;  }
+
+        ManualResetEventSlim ReceivingData { get; set; }
+
+        ManualResetEventSlim Timeout { get; set; }
+
+        ManualResetEventSlim WritingData { get; set; }
+
+        Socket Listener { get; set; }
+
+		void ChangeBufferSize(int size);
+
+		string Info();
+
+		void ResetDataReceiver();
+
+    }
+
+}
